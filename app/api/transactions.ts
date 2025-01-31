b080ca994c5cbd63c95743432d55ec18dc763a5d
Somethea Siek <sometheasiekswx@gmail.com>
2025-02-01 00:02:49 +1100

    Add fetchAllTransactionsPeriodLast12Months

--- app/api/transactions.ts
+++ app/api/transactions.ts
@@ -1,6 +1,56 @@
 import axiosTransactionsTracker from "@/app/lib/axios/axiosTransactionsTracker";
 import {Status} from "@/app/lib/definitions";
 
+function transformSpendingData(data: { _id: { year: number, month: number }, totalSpent: number }[]) {
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    // Create a map of revenue for quick lookup
+    const revenueMap = new Map(data.map(item => [`${item._id.year}-${item._id.month}`, item.totalSpent]));
+
+    // Generate last 12 months' data
+    const result = [];
+    const now = new Date();
+    let currentYear = now.getFullYear();
+    let currentMonth = now.getMonth() + 1; // JS months are 0-based
+
+    for (let i = 0; i < 12; i++) {
+        const key = `${currentYear}-${currentMonth}`;
+        result.push({
+            month: monthNames[currentMonth - 1], year: currentYear, revenue: revenueMap.get(key) || 0
+        });
+
+        // Move to the previous month
+        currentMonth--;
+        if (currentMonth === 0) {
+            currentMonth = 12;
+            currentYear--;
+        }
+    }
+
+    return result;
+}
+
+
+export async function fetchAllTransactionsPeriodLast12Months() {
+    try {
+        const startTime = performance.now();
+
+        const response = await axiosTransactionsTracker.get(`/transactions/all/period/last-12-months`)
+
+        const endTime = performance.now();
+        const duration = endTime - startTime;
+        console.log(`fetchAllTransactionsPeriodLast12Months completed in ${duration.toFixed(2)} ms`);
+
+        // TODO: Check if data is empty first
+
+        return transformSpendingData(response.data.totalSpending);
+    } catch (error) {
+        const message = 'Failed to fetchAllTransactionsPeriodLast12Months\n' + error;
+        console.error(message);
+        throw new Error(message);
+    }
+}
+
 export async function fetchAllTransactionsStatusCount() {
     try {
         const startTime = performance.now();
@@ -13,7 +63,7 @@ export async function fetchAllTransactionsStatusCount() {
 
         return response.data;
     } catch (error) {
-        const message = 'Failed to fetchAllTransactions\n' + error;
+        const message = 'Failed to fetchAllTransactionsStatusCount\n' + error;
         console.error(message);
         throw new Error(message);
     }
